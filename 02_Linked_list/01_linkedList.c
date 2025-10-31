fda95fc77ba8c2c81c553f6320420be6a7dc3fb9
Iman-Datta <dattaiman56@gmail.com>
2025-10-31 13:17:31 +0530

    linked list

--- 02_Linked_list/01_linkedList.c
+++ 02_Linked_list/01_linkedList.c
@@ -4,21 +4,12 @@
 typedef struct Node{
     int data;
     struct Node *next;
-};
-typedef struct Node node; // Allias
+}node;
 
-// typedef int apple; 
-// apple a;
+node* head = NULL;
 
-node *head = NULL; // Global
-
-node* createNode(int val){
-    node* newNode = (node *)malloc(sizeof(node));
-
-    if (newNode == NULL) {
-        printf("Memory allocation failed.\n");
-        return 0;
-    }
+node *createNode(int val){
+    node *newNode = (node*)malloc(sizeof(node));
 
     newNode->data = val;
     newNode->next = NULL;
@@ -26,173 +17,203 @@ node* createNode(int val){
     return newNode;
 }
 
-void insertBegining(int value){
-    node* newNode = createNode(value);
+void insertBegining(int val){
+    node *newNode = createNode(val);
     if (head == NULL){
         head = newNode;
     }
-    else{
+    else {
         newNode->next = head;
         head = newNode;
     }
 }
 
-void insertEnd(int value) {
-    node *newNode = createNode(value);
-    node *temp = head;
-    if (head == NULL){
-        head = newNode;
-    }
-    else{
-        while (temp->next != NULL){
-            temp =  temp->next;
-        }
-    temp->next = newNode;
-    }
-}
-
-void insertAtAnyNode(int value, int position) {
-    if (position < 1){
-    printf("Invalid node position.\n");
-    return;
-    }
-
-    if (position == 1){
-        insertBegining(value);
-        return;
-    }
-
-    node *newNode = createNode(value);
-    node *temp = head, *pre_temp = head;
-
-    while(temp != NULL && position > 1) {
-        pre_temp = temp;
-        temp = temp->next;
-
-        position --;
-    }
-    pre_temp->next = newNode;
-    newNode->next = temp;
-}
-
-void display() {
-    node *temp = head; 
+void display(){
+    node* temp = head;
     if (head == NULL) {
         printf("Your linked list is empty\n");
         return;
     }
-
+    
     printf("Your Linked List: \n");
-    while (temp != NULL){
+    while (temp != NULL) {
         printf("%d -->>", temp->data);
-        temp =  temp->next;
+        temp = temp->next;
     }
 }
 
-void deleteBeginning (){
+void insertEnd(int val) {
+    node *newNode = createNode(val);
+    node *temp = head;
+
+    if (head == NULL) {
+        head = newNode;
+    }
+    else {
+        while (temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void insertAtAnyNode(int val, int pos){
+    int count = 0;
+    node* temp = head, *preTemp = head;
+    while (temp != NULL) {
+        count ++;
+        temp = temp->next;
+    }
+    temp = head;
+
+    node *newNode = createNode(val);
+
+    if (pos < 0 || pos > count) {
+        printf("Invalid node position.\n");
+        return;
+    }
+    else if (head == NULL){
+        head = newNode;
+        return;
+    }
+    else if (pos == 0) {
+        insertBegining(val);
+        return;
+    }
+    else {
+        while (pos > 0 && temp != NULL){
+            preTemp = temp;
+            temp = temp->next;
+            pos --;
+        }
+        newNode->next = temp;
+        preTemp->next = newNode;
+        return;
+    }
+
+}
+
+void deleteBeginning(){
+    node *temp = head;
     if (head == NULL) {
         printf("List is already empty.\n");
         return;
     }
 
-    node* temp = head;
     head = head->next;
     free(temp);
 }
 
 void deleteEnd(){
-    node *temp = head, *pre_temp = head;
-    if (temp == NULL) {
+    node *temp = head, *preTemp = head;
+    if (head == NULL) {
         printf("List is already empty.\n");
         return;
     }
 
-        if (temp->next == NULL) {
+    if (temp->next == NULL){
         free(temp);
         head = NULL;
         return;
     }
-
     while (temp->next != NULL){
-        pre_temp = temp;
+        preTemp = temp;
         temp = temp->next;
     }
 
-    pre_temp->next = NULL;
+    preTemp->next = NULL;
     free(temp);
 }
 
-void deleteAnyNode(int position) {
+void deleteAnyNode(int pos){
     node *temp = head, *past =  head, *future = NULL;
-    if (temp == NULL) {
-        printf ("List is already empty.\n");
+    if (pos < 0) {
+        printf ("Invalid position.\n");
         return;
     }
-    
-    if (position < 1) {
-        printf("Invalid position");
-        return;
-    } 
-
-    if (position == 1) {
-        deleteBeginning(position);
+    else if (pos == 0) {
+        deleteBeginning();
         return;
     }
-
-    while (position > 1) {
-        past = temp;
-        temp = temp->next;
-
-        position --;
+    else{
+        while (pos != 0  && temp != NULL){
+            past = temp;
+            temp = temp->next;
+            pos --;
+        }
+        if (temp == NULL){
+            printf("Position out of range.\n");
+            return;
+        }
+        past->next = temp->next;
+        free(temp);
+        return;
     }
-
-    future = temp->next->next;
-    past->next = future;
-    free(temp);
 }
 
-void searchByValue(int val){
-    node *temp =  head;
-    int pos = 1, status = 0;
+void searchByValue(int val) {
+    node *temp = head;
+    int pos = 0, status = 0;
 
-    if (temp == NULL) {
+    if (head == NULL) {
         printf("List is already empty.\n");
+        return;
     }
 
-    while (temp->next != NULL) {
+    while(temp != NULL){
         if (temp->data == val) {
-            printf("Your data is found in %d node. The addrees of that node is %d \n", pos, temp->next);
+            printf("Your data is found at node position %d", pos);
             status = 1;
         }
-        pos ++;
         temp = temp->next;
+        pos ++;
     }
-    if (status == 0) {
-        printf ("Value %d not found in the list.\n", val);
+    if (status == 0){
+        printf("Not found");
     }
-    
+    return;
 }
 
-void searchByposition(int pos){
+void searchByposition(int pos) {
     node *temp = head;
+    int tempPos = pos;
 
-    if (temp == NULL) {
-        printf ("List is already empty.\n");
-    }
-
-    while (pos > 1) {
+    while (pos > 0 && temp != NULL){
         temp = temp->next;
         pos --;
     }
-
-    printf("At %d number node: Your data is %d \n", pos, temp->data);
+    if (temp == NULL) {
+        printf("Position %d not found.\n", tempPos);
+        return;
+    }
+    printf("At %d number node: Your data is %d \n", tempPos, temp->data);
 }
 
+void reverseList() {
+    node *prev = NULL;
+    node *current = head;
+    node *next = NULL;
+
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    head = prev;
+    printf("Linked list reversed successfully.\n");
+}
+
+
 int main () {
-    // node * head == NULL;
     int choice, value, position;
-    while (1)
-    { // Puts () should be use
+    while (1){
         printf("\nPress 1: insert data at the starting\n");
         printf("Press 2: insert data at the end\n");
         printf("Press 3: insert data at any node\n");
@@ -202,51 +223,73 @@ int main () {
         printf("Press 7: Delete at any node\n");
         printf("Press 8: Search by value\n");
         printf("Press 9: Search by position\n");
+        printf("Press 10: reverse the linked list\n");
+        printf("Press 11: Exsit the function\n");
 
         printf("Enter your choice: ");
         scanf("%d", &choice);
-    
-        switch (choice)
-        {
-        case 1:
+
+        switch (choice) {
+            case 1:
             printf("Enter the your data: ");
             scanf("%d", &value);
             insertBegining(value);
             break;
-        case 2:
+
+            case 2:
             printf("Enter the your data: ");
             scanf("%d", &value);
             insertEnd(value);
             break;
-        case 3:
+
+            case 3:
             printf("Enter the your data: ");
             scanf("%d", &value);
             printf("Enter the number of node: ");
             scanf("%d", &position);
             insertAtAnyNode(value, position);
             break;
-        case 4:
+
+            case 4:
             display();
             break;
-        case 5:
+
+            case 5:
             deleteBeginning();
             break;
-        case 6:
+
+            case 6:
             deleteEnd();
             break;
-        case 7:
+
+            case 7:
             printf("Enter the number of node: ");
             scanf("%d", &position);
             deleteAnyNode(position);
-        case 8:
+            break;
+
+            case 8:
             printf("Enter your data: ");
             scanf("%d", &value);
             searchByValue(value);
-        case 9:
+            break;
+
+            case 9:
             printf("Enter the node position: ");
             scanf("%d", &position);
             searchByposition(position);
-        default:
+            break;
+
+            case 10:
+            reverseList();
+            break;
+
+            case 11:
+            printf("Exiting the program.\n");
+            return 0;
+
+            default:
+            printf("Invalid choice. Please try again.\n");
             break;
         }
     }
